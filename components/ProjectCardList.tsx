8216b8250fc8bc17888735bcd7bdf6bb66d80a47
Ufedojo <ufedojoatabo@gmail.com>
2022-12-05 23:27:50 +0100

    fix to dark mode + spacing

--- components/ProjectCardList.tsx
+++ components/ProjectCardList.tsx
@@ -3,7 +3,7 @@ import { Project } from "./types";
 
 export const ProjectCardList = ({ projects }: { projects: Project[] }) => {
   return (
-    <div className="grid md:grid-cols-2 max-w-4xl w-full mx-auto gap-5 gap-y-8">
+    <div className="grid md:grid-cols-2 max-w-4xl w-full mx-auto gap-5 gap-y-10 md:gap-y-14">
       {projects.map((project) => {
         return <ProjectCard key={project.title} {...project} />;
       })}
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,23 +6,25 @@ import { Header } from "../components/Header";
 import { Hero } from "../components/Hero";
 import { ProjectCardList } from "../components/ProjectCardList";
 import { Project } from "../components/types";
-import projectsJson from "../projects.json"
+import projectsJson from "../projects.json";
 
 const Home: NextPage = () => {
   const projects: Project[] = projectsJson;
   return (
-    <div className="relative">
-      <Head>
-        <title>Ige Ayodeji&apos;s portfolio</title>
-        <meta name='description' content="Ige Ayodeji's portfolio" />
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-      <Header />
-      <Hero />
-      <section id="works" className='page-side-padding py-8'>
-        <ProjectCardList projects={projects} />
-      </section>
-      <Footer />
+    <div className="dark">
+      <div className='relative dark:bg-black dark:text-white'>
+        <Head>
+          <title>Ige Ayodeji&apos;s portfolio</title>
+          <meta name='description' content="Ige Ayodeji's portfolio" />
+          <link rel='icon' href='/favicon.ico' />
+        </Head>
+        <Header />
+        <Hero />
+        <section id='works' className='page-side-padding py-8'>
+          <ProjectCardList projects={projects} />
+        </section>
+        <Footer />
+      </div>
     </div>
   );
 };
